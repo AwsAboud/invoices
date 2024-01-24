abad8fa9c24d6ce0ad16894a950b858aa1645bbc
AwsAboud <aws.amin.aboud@gmail.com>
2024-01-24 13:03:48 +0300

    section-management

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -12,7 +12,7 @@ class InvoiceController extends Controller
      */
     public function index()
     {
-        return view('invoice.index');
+        return view('invoices.index');
     }
 
     /**
